1c0670546b01198b9de03170a5253c9eec81d599
Jen Zimski <58517278+jlzimski@users.noreply.github.com>
2022-08-31 13:19:44 -0400

    initial commit

---
+++ controllers/usercontroller.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const { UniqueConstraintError } = require("sequelize/lib/errors");
+const { UserModel } = require("../models");
+
+router.post("/register", async (req, res) => {
+    let { email, password } = req.body.user;
+    try {
+        const User = await UserModel.create({
+            email,
+            password,
+        });
+        res.status(201).json({
+            message: "User successfully registered",
+            user: User,
+        });
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: "Email already in use",
+            });
+        } else {
+            res.status(500).json({
+                message: "Failed to register user",
+            });
+        }
+    }
+});
+
+router.post("/login", async (req, res) => {
+    let { email, password } = req.body.user;
+    await UserModel.findOne({
+        where: {
+            email: email,
+        },
+    });
+});
+
+module.exports = router;
