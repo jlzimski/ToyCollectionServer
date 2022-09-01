6b0f1b2d3b55d73319e62b553f485f879541a9b6
Jen Zimski <58517278+jlzimski@users.noreply.github.com>
2022-09-01 01:17:27 -0400

    user complete

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,17 +1,23 @@
 const router = require("express").Router();
 const { UniqueConstraintError } = require("sequelize/lib/errors");
 const { UserModel } = require("../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
     let { email, password } = req.body.user;
     try {
         const User = await UserModel.create({
             email,
-            password,
+            password: bcrypt.hashSync(password, 13),
         });
+
+        let token = jwt.sign({ id: User.id }, process.env.JWT_SECRET, {expiresIn: 60 * 60* 24});
+
         res.status(201).json({
             message: "User successfully registered",
             user: User,
+            sessionToken: token
         });
     } catch (err) {
         if (err instanceof UniqueConstraintError) {
@@ -28,11 +34,36 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
     let { email, password } = req.body.user;
-    await UserModel.findOne({
+    try {
+    const loginUser = await UserModel.findOne({
         where: {
             email: email,
         },
     });
+    if (loginUser) {
+        let passwordComparison = await bcrypt.compare(password, loginUser.password);
+        if (passwordComparison) {
+        let token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+    res.status(200).json({
+        user: loginUser,
+        message: "User successfully logged in!",
+        sessionToken: token
+    });
+    } else {
+        res.status(401).json({
+            message: "Incorrect email or password"
+        })
+    }
+    } else {
+        res.status(401).json({
+            message: "Incorrect email or password"
+        });
+    }
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to log user in"
+        })
+    }
 });
 
 module.exports = router;
